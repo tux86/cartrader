540c4bc01e95010fd2326905262f6603d22f87f5
Walid Karray <walid.karray@gmail.com>
2023-03-14 22:21:43 +0100

    section 4

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -1,14 +1,45 @@
+<script setup>
+const modal = ref({
+  make: false,
+  location: false,
+  price: false
+})
+
+const city = ref('')
+
+const updateModal = (key) => {
+  modal.value[key] = !modal.value[key]
+}
+
+const route = useRoute()
+
+const onChangeLocation = () => {
+  if (!city.value) return ;
+
+  if(!isNaN(Number(city.value))){
+    throw createError({
+      statusCode: 400,
+      message: "Invalid city format"
+    })
+  }
+
+  updateModal('location');
+  navigateTo(`/city/${city.value}/car/${route.params.make}`);
+  city.value = ''
+}
+</script>
+
 <template>
   <div class="shadow border w-64 mr-10 z-30 h-[190px]">
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Location</h3>
-      <h3 class="text-blue-400 capitalize">Toronto</h3>
-      <!-- <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
-            <input type="text" class="border p-1 rounded" />
-            <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">
+      <h3 @click="updateModal('location')" class="text-blue-400 capitalize">{{route.params.city}}</h3>
+     <div v-if="modal.location" class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
+            <input type="text" class="border p-1 rounded" v-model="city" />
+            <button @click="onChangeLocation" class="bg-blue-400 w-full mt-2 rounded text-white p-1">
               Apply
             </button>
-          </div> -->
+          </div>
     </div>
 
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
