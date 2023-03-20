2df9e2a6f939ee6fce37619d2ba3c14068704d7a
Walid Karray <walid.karray@gmail.com>
2023-03-20 22:17:47 +0100

    section 6.37

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -1,4 +1,8 @@
 <script setup>
+
+import {useCars} from "~/composables/useCars";
+
+const {makes} = useCars()
 const modal = ref({
   make: false,
   location: false,
@@ -27,10 +31,18 @@ const onChangeLocation = () => {
   navigateTo(`/city/${city.value}/car/${route.params.make}`);
   city.value = ''
 }
+
+
+const onChangeMake = (make) => {
+  updateModal('make')
+  navigateTo(`/city/${route.params.city}/car/${make}`)
+}
 </script>
 
 <template>
   <div class="shadow border w-64 mr-10 z-30 h-[190px]">
+
+    <!-- LOCATION START -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Location</h3>
       <h3 @click="updateModal('location')" class="text-blue-400 capitalize">{{route.params.city}}</h3>
@@ -41,11 +53,19 @@ const onChangeLocation = () => {
             </button>
           </div>
     </div>
+    <!-- LOCATION END -->
 
+    <!-- MAKE START -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Make</h3>
-      <h3 class="text-blue-400 capitalize">Toyota</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('make')">{{route.params.make || 'Any'}}</h3>
+
+      <div class="absolute border shadow left-56 p-5 top-1 -m-1 w-[600px] flex justify-between flex-wrap bg-white" v-if="modal.make">
+        <h4 v-for="make in makes" :key="make" class="w-1/3" @click="onChangeMake(make)">{{make}}</h4>
+      </div>
     </div>
+    <!-- MAKE END -->
+
 
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Price</h3>
--- composables/useCars.ts
+++ composables/useCars.ts
@@ -1,6 +1,8 @@
 import cars from "@/data/cars.json"
+import makes from "@/data/makes.json"
 export const useCars = () => {
  return {
      cars,
+     makes
  }
 }
