07d5999a729dac590ebca39f64b7f9ecd8148b9b
Walid Karray <walid.karray@gmail.com>
2023-03-22 16:20:56 +0100

    section 7.45

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,3 +1,18 @@
+<script setup>
+
+const user = useSupabaseUser();
+const supabase = useSupabaseClient();
+
+const logout = () => {
+  const {error} = supabase.auth.signOut()
+  if (error) {
+    return console.log(error)
+  }
+  user.value = null
+  navigateTo('/')
+}
+
+</script>
 <template>
   <header
     class="
@@ -15,5 +30,11 @@
     "
   >
     <NuxtLink class="text-3xl font-mono" to="/">cartrader</NuxtLink>
+
+    <div v-if="user" class="flex">
+      <NuxtLink to="/profile/listings" class="mr-5">Profile</NuxtLink>
+      <p class="cursor-pointer" @click="logout">Logout</p>
+    </div>
+    <NuxtLink v-else to="/login">Login</NuxtLink>
   </header>
 </template>
